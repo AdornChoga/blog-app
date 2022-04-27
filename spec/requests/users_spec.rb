b21bb826f965c62acc70fe4030fc38d9b2e0f654
Adorn Choga <adornchoga@gmail.com>
2022-04-27 13:54:09 +0200

    Added two more tests for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,25 +1,37 @@
 require 'rails_helper'
 
 describe 'Users index', type: :request do
-  it 'should return a 200 response status code' do
+  before(:each) do
     get '/users'
+  end
+
+  it 'should return a 200 response status code' do
     expect(response).to have_http_status 200
   end
 
   it 'should render index template' do
-    get '/users'
     expect(response).to render_template(:index)
+  end
+
+  it 'include correct text in the body' do
+    expect(response.body).to include 'Here is a list of all users...'
   end
 end
 
 describe 'Users show', type: :request do
-  it 'should return a 200 response status code' do
+  before(:each) do
     get '/users/20'
+  end
+
+  it 'should return a 200 response status code' do
     expect(response).to have_http_status 200
   end
 
   it 'should render show template' do
-    get '/users/20'
     expect(response).to render_template(:show)
+  end
+
+  it 'include correct text in the body' do
+    expect(response.body).to include 'Here is a user with id: 20'
   end
 end
