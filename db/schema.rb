e3beb66d2344e5e7c93591c7de20ba1124df743a
Adorn Choga <adornchoga@gmail.com>
2022-04-29 07:12:06 +0200

    Associated user to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_200102) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_050932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_200102) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_200102) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
