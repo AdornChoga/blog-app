522f2f1c2efcac0ce2e5ad421cfbe3e33b75474d
Adorn Choga <adornchoga@gmail.com>
2022-05-02 21:56:45 +0200

    Created update_likes_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def update_likes_counter
+    post = Post.find(post_id)
+    post_likes = post.likes_counter
+    post_likes = 0 unless post_likes
+    post.update(likes_counter: post_likes + 1)
+  end
 end
