35a691cf33345c140f478170cd34cb80393c2739
Adorn Choga <adornchoga@gmail.com>
2022-05-05 21:21:11 +0200

    Created form for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,27 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:success] = 'Post was successfully created.'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:danger] = 'Post was not created.'
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
