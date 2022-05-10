0cfbe250c3a1f419d48a6caadae9a1e9acce8d11
Adorn Choga <adornchoga@gmail.com>
2022-05-10 13:27:55 +0200

    Add bio and photo sign-up fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,17 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password_confirmation, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(
+        :name, :email, :bio, :photo,
+        :password_confirmation, :password
+      )
+    end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :surname, :email, :password_confirmation, :password, :current_password)
+      u.permit(
+        :name, :email, :bio, :photo,
+        :password_confirmation, :password, :current_password
+      )
     end
   end
 end
