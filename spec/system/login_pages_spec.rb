5c5ee07b6267ccd7948442abf1d058f83df2f47e
matovu-farid <matovu90@gmail.com>
2022-05-11 16:32:41 +0300

    Add interraction context

--- spec/system/login_pages_spec.rb
+++ spec/system/login_pages_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe "LoginPages", type: :system do
   before do
     driven_by(:rack_test)
   end
+  before(:each){visit "/users/sign_in"}
+
   context "Login page content" do
-    before(:each){visit "/users/sign_in"}
     it "should have email field" do
       expect(page).to have_content("Email")
     end
@@ -15,5 +16,17 @@ RSpec.describe "LoginPages", type: :system do
     it "should have login button" do
       expect(page).to have_content("Log in")
     end
+  end
+  context "Interractions" do
+    it "should throw an error if the inputs are not filled in" do
+      click_button "Log in"
+      expect {page}.to raise_error(ArgumentError)
+    end
+    # it "should have password field" do
+    #   expect(page).to have_content("Password")
+    # end
+    # it "should have login button" do
+    #   expect(page).to have_content("Log in")
+    # end
   end
 end
